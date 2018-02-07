f93232b3665368ee6d32de936c583df13ee50474
jkahwati <jesuskahwati@hotmail.com>
2018-02-07 17:23:57 -0300

    Adding interactive menu

--- main.rb
+++ main.rb
@@ -1,14 +1,35 @@
 require "./lib/article"
 require 'json'
+continue = true
+while(continue)
+	p '_____________________________________________________'
+	puts "Selecciona una opción:
+	  1: Scenario 1
+	  2: Scenario 2
+	  3: Scenario 3
+	  4: Scenario 4
+	  5: Agregar un nuevo scenario (json)
+	  6: Pruebas automáticas (rspec)
+	  7: Salir"
+	 p '_____________________________________________________'
 
-file = File.read('articles_recomendation.json')
-data_hash = JSON.parse(file)
-
-article = Article.new
-
-article.recomended(data_hash)
-
-
-
-
-
+	STDOUT.flush  
+	option = gets.chomp 
+		case option.to_i
+		when 1..4 then
+			file = File.read("scenario#{option}.json")
+			data_hash = JSON.parse(file)
+			article = Article.new
+			article.recomended(data_hash)
+		when 5 then
+			p "Introduzca json: "
+			new_scenario = gets.chomp 
+			data_hash = JSON.parse(new_scenario)
+			article = Article.new
+			article.recomended(data_hash)
+		when 6 then
+			system('bundle exec rspec')
+		else
+			continue=false
+		end
+end
